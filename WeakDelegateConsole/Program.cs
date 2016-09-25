c4d024aebc2a01aaf0d82338f0e8303668b3617b
Ирина Новикова <pryanyi.santehnik@gmail.com>
2016-09-25 04:50:30 +0300

    2

---
+++ WeakDelegateConsole/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeakDelegateConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Source
+    {
+        public event Action<int> complited;
+    }
+
+    public class Listner
+    {
+
+    }
+
+    public class WeakDelegate
+    {
+        private WeakReference Target;
+        private MethodInfo Method;
+
+        public WeakDelegate(Delegate d)
+        {
+            Target = new WeakReference(d.Target);
+            Method = d.Method;
+            d = null;
+        }
+
+        public Delegate Weak
+        {
+            get
+            {
+              
+            }
+        }
+            
+    }
+}
