e172a1ed4a4d59b5eecc276ae3eb410a7cefd9d9
AliaksandrYenza <sashayenza1996@gmail.com>
2016-10-01 16:34:12 +0300

    from Poland
    111

---
+++ WeakDelegateConsole/UnitTest/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeakDelegateConsole;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public void ClearGC()
+        {
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+            GC.WaitForFullGCComplete(100);
+            GC.WaitForPendingFinalizers();
+        }
+
+        static bool ListenerWasDestroyed;
+
+        public class TestListener : Listener
+        {
+            ~TestListener()
+            {
+                ListenerWasDestroyed = true;
+            }
+        }
+
+        [TestMethod]
+        public void TestHardReferenceListenerDontDisappear()
+        {
+            ListenerWasDestroyed = false;
+            Source source = new Source();
+            Listener listener = new TestListener();
+            source.Completed += listener.Handler;
+            listener = null;
+            ClearGC();
+            Assert.IsFalse(ListenerWasDestroyed);
+        }
+
+        [TestMethod]
+        public void TestWeakReferenceListenerDisappear()
+        {
+            ListenerWasDestroyed = false;
+            Source source = new Source();
+            Listener listener = new TestListener();
+            WeakDelegate weakDelegate = new WeakDelegate((Action<int>)listener.Handler);
+            source.Completed += (Action<int>)weakDelegate.Weak;
+            listener = null;
+            ClearGC();
+        }
+
+        [TestMethod]
+        public void TestWeakReferenceDisappearing()
+        {
+            Source source = new Source();
+            Listener listener = new Listener();
+            WeakDelegate weakDelegate = new WeakDelegate((Action<int>)listener.Handler);
+            source.Completed += (Action<int>)weakDelegate.Weak;
+            listener = null;
+            ClearGC();
+            Assert.IsTrue(weakDelegate.TargetIsNull);
+
+        }
+    }
+}
